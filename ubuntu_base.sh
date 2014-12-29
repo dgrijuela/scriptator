c9734c56f80dd30a3789dda7996f7609aaa06581
Daniel Grijuela <danielgrijuela@gmail.com>
2014-12-29 17:41:54 +0100

    Add git installation

--- ubuntu_base.sh
+++ ubuntu_base.sh
@@ -3,11 +3,14 @@
 # Exit script immediately on first error.
 set -e 
 
+printf "Adding necessary repositories"
+add-apt-repository ppa:git-core/ppa > /dev/null 2>&1 # last git version
+
 printf "Updating packages list"
 apt-get -qq update # qq is no output except for errors (like >/dev/null)
 
 printf "Installing base packages"
-apt-get -y install curl build-essential libssl-dev libcurl4-gnutls-dev libexpat1-dev wget > /dev/null 2>&1
+apt-get -y install curl build-essential libssl-dev libcurl4-gnutls-dev libexpat1-dev wget git > /dev/null 2>&1
 
 printf "Editing .bashrc to always arrive at /var/www"
 sh -c 'printf "cd /var/www/ >> ~/.bashrc"'
