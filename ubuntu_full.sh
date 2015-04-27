3bf8344e09efe9dfb9a859bce602313522a4e4a9
Daniel Grijuela <danielgrijuela@gmail.com>
2015-04-27 22:13:07 +0200

    Add tmux, reorder and fix other things

--- ubuntu_full.sh
+++ ubuntu_full.sh
@@ -19,19 +19,23 @@ apt-get -y purge thunderbird nautilus-sendto empathy
 apt-get -y autoremove
 apt-get -y autoclean
 
+echo "Installing base packages..."
+apt-get -y install curl libpq-dev freetds-dev tmux libmagickwand-dev libmysqlclient-dev build-essential python python-dev cmake libssl-dev libcurl4-gnutls-dev libexpat1-dev wget vim-gtk git whois
+
 echo "Adding necessary repositories..."
 add-apt-repository -y ppa:git-core/ppa # last git version
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -# google key
 curl -sL https://deb.nodesource.com/setup | sudo bash - # node repo
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' # chrome repo
 apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E # mit key
-add-apt-repository -y "deb http://linux.dropbox.com/ubuntu $(lsb_release -cs) main" # dropbox repo
 
 echo "Remember to do apt-get update (it fails sometimes)..."
-#apt-get update
+apt-get update
 
-echo "Installing base packages..."
-apt-get -y install curl libpq-dev freetds-dev libmagickwand-dev libmysqlclient-dev build-essential python python-dev cmake libssl-dev libcurl4-gnutls-dev libexpat1-dev wget vim-gtk git whois
+echo "Installing more basic packages..."
+apt-get install git
+
+echo "Installing Meteor..."
+curl https://install.meteor.com/ | sh
 
 echo "Installing Terminator (great console)..."
 apt-get install -y terminator
@@ -64,6 +68,7 @@ echo "Installing Virtualbox..."
 apt-get install dkms # dependency
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add - # Oracle public key
 sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian trusty contrib" >> /etc/apt/sources.list.d/virtualbox.list' # Add the ppa
+apt-get update
 apt-get install virtualbox-4.3
 
 echo "Installing Vagrant..."
